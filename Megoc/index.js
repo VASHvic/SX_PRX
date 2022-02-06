d453631462272297f6074e0de1d48155a876c6f8
VASHvic <vforvanish@gmail.com>
2022-02-07 00:47:16 +0100

    added modal windows

--- Megoc/index.js
+++ Megoc/index.js
@@ -5,40 +5,85 @@ const proximsConcerts = document.getElementById('proxims-concerts');
 const main = document.querySelector('main');
 const media = window.matchMedia('(max-width: 960px)');
 const footer = document.querySelector('footer');
-let checkModal = false;
+const cantar = document.getElementById('cantar');
+
+const estat = {
+  modal: false,
+  quiSom: true,
+  pasio: true,
+  concerts: false,
+  cantar: false,
+};
 
 media.addEventListener('change', refresh);
 
 function mostraConcerts() {
-  checkModal = true;
+  if (estat.cantar) {
+    estat.concerts = true;
+    estat.modal = true;
+    estat.cantar = false;
+    cantar.style.display = 'none';
+    proximsConcerts.style.display = 'block';
+    proximsConcerts.classList.remove('animate__fadeOut');
+    proximsConcerts.addEventListener('click', amagaConcerts);
+    return;
+  }
+  estat.concerts = true;
+  estat.pasio = false;
+  estat.quiSom = false;
+  estat.modal = true;
   quiSom.classList.add('animate__fadeOut');
   pasio.classList.add('animate__fadeOut');
   setTimeout(() => {
     quiSom.style.display = 'none';
     pasio.style.display = 'none';
     main.style.display = 'flex';
-    proximsConcerts.classList.add('animate__fadeIn');
+    proximsConcerts.classList.remove('animate__fadeOut');
     proximsConcerts.style.display = 'block';
+    proximsConcerts.classList.add('yellow-shadow2');
     proximsConcerts.addEventListener('click', amagaConcerts);
-  }, 1000);
+  }, 500);
 }
 
 function amagaConcerts() {
+  estat.concerts = false;
+  estat.pasio = true;
+  estat.quiSom = true;
+  estat.modal = true;
   quiSom.classList.remove('animate__fadeOut');
   pasio.classList.remove('animate__fadeOut');
-
+  quiSom.classList.add('animate__fadeIn');
+  pasio.classList.add('animate__fadeIn');
+  proximsConcerts.classList.add('animate__fadeOut');
+  proximsConcerts.classList.add('animate__fadeOut');
   main.style.display = 'grid';
   proximsConcerts.style.display = 'none';
-  quiSom.style.display = 'block';
-  pasio.style.display = 'block';
-  //   pasio.style.position = 'relative';
-  pasioH1.style.display = 'none';
-  //   quiSom.classList.add('animate__fadeIn');
-  //   pasio.classList.add('animate__fadeIn');
+  quiSom.style.display = 'flex';
+  pasio.style.display = 'flex';
+}
+
+function tagradaCantar() {
+  if (estat.concerts) {
+    estat.concerts = false;
+    estat.modal = true;
+    estat.cantar = true;
+    proximsConcerts.style.display = 'none';
+    cantar.style.display = 'block';
+  }
+  if (estat.quiSom) {
+    estat.quiSom = false;
+    estat.pasio = false;
+    estat.modal = true;
+    estat.cantar = true;
+    main.style.display = 'flex';
+    quiSom.style.display = 'none';
+    pasio.style.display = 'none';
+    cantar.style.display = 'flex';
+  }
 }
 
 function refresh() {
-  if (checkModal) {
+  if (estat.modal) {
     main.style.display = 'none';
     footer.style.display = 'none';
     location.reload();
